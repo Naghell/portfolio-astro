0061abe5c8ca4704bf662921ef45ea6955507c4e
Renzo <naghybusiness@gmail.com>
2024-08-07 09:02:10 -0300

    Optimization

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter Variable", "ui-sans-serif", "system-ui"],
+    },
     extend: {
       colors: {
         lightBackground: "#FCFCFD",
